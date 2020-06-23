6fdc901dafb4f8dac50c46181bcf13a06e099db2
Alex Joel <alexjoeldelgado@gmail.com>
2020-06-22 21:52:48 -0400

    Updated Portfolio Objects

--- routes/portObjects.js
+++ routes/portObjects.js
@@ -1,59 +1,90 @@
 const portObjects = [
   {
+    name: "Starfinder Starter",
+    screenshot: "/images/SS-ss.PNG",
+    urlD: "https://starfinder-starter.herokuapp.com/",
+    urlG: "https://github.com/Auretus/DandD-Character-Generator",
+    dscr: "A play-along application for the Starfinder DnD Game.",
+    tech: "React, MongoDB, Express, Node.js, JWT, Axios, Materialize"
+  },{
     name: "Portfolio Website",
     screenshot: "/images/PP-ss.PNG",
-    url: "https://github.com/alexjoeldelgado/professional-portfolio",
-    dscr: "This here is the repository for the website you are on!"
+    urlD: "http://www.salazarsoftware.com/",
+    urlG: "https://github.com/alexjoeldelgado/professional-portfolio",
+    dscr: "This here is the repository for the website you are on!",
+    tech: "Express Handlebars, Node.js, HTML, CSS"
+  },{
+    name: "PWA Budget Tracker",
+    screenshot: "/images/BT-ss.PNG",
+    urlD: "https://protected-harbor-57711.herokuapp.com/",
+    urlG: "https://github.com/alexjoeldelgado/PWA-budget-tracker",
+    dscr: "Progressive Web App that allows a user to keep track of their budget while travelling",
+    tech: "PWA efficiency, IndexedDb, MongoDB"
   },{
     name: "VEG-HEAD",
     screenshot: "/images/VH-ss.jpg",
-    url: "https://alexjoeldelgado.github.io/veg-head/",
-    dscr: "A web application designed for Vegetarians to search for local vegetarian-friendly restaurants and vegetarian recipes."
+    urlG: "https://github.com/yarocruz/veg-head",
+    urlD: "https://yarocruz.github.io/veg-head/",
+    dscr: "A web application designed for Vegetarians to search for local vegetarian-friendly restaurants and vegetarian recipes.",
+    tech: "AJAX, HTML, CSS"
   },{
     name: "Workout Tracker",
     screenshot: "/images/WT-ss.PNG",
-    url: "https://intense-taiga-75007.herokuapp.com/",
-    dscr: "This application will allow a consumer to reach their fitness goals by tracking their workout progress."
+    urlD: "https://intense-taiga-75007.herokuapp.com/",
+    urlG: "https://github.com/alexjoeldelgado/WorkoutTracker",
+    dscr: "This application will allow a consumer to reach their fitness goals by tracking their workout progress.",
+    tech: "Express, MongoDB, SEO"
   },{
     name: "Aloha",
     screenshot: "/images/AIM-ss.PNG",
-    url: "https://hidden-sea-12674.herokuapp.com/",
-    dscr: "A web application designed for real-time instant messaging. Not Functional at the Moment"
-  },{
-    name: "Employee Tracker",
-    screenshot: "/images/ET-ss.PNG",
-    url: "https://github.com/alexjoeldelgado/EmployeeTracker",
-    dscr: "A Node application designed to add, delete, and update employee information for a database."
-  },{
+    urlD: "https://hidden-sea-12674.herokuapp.com/",
+    urlG: "https://github.com/Auretus/Aloha",
+    dscr: "A web application designed for real-time instant messaging.",
+    tech: "Express, MySQL, Socket.io, Passport Authentication"
+  },
+  // {
+  //   name: "Employee Tracker",
+  //   screenshot: "/images/ET-ss.PNG",
+  //   urlG: "https://github.com/alexjoeldelgado/EmployeeTracker",
+  //   dscr: "A Node application designed to add, delete, and update employee information for a database.",
+  //   tech: "Express, Node.js"
+  // },
+  {
     name: "Burger: Practice with Handlebars",
     screenshot: "/images/BB-ss.PNG",
-    url: "https://lit-coast-42478.herokuapp.com/",
-    dscr: "A web application designed to show expertise using node express handlebars."
+    urlD: "https://lit-coast-42478.herokuapp.com/",
+    urlG: "https://github.com/alexjoeldelgado/burger",
+    dscr: "A web application designed to show expertise using node express handlebars.",
+    tech: "Express Handlebars, CSS, HTML"
   },{
     name: "Note Taker",
     screenshot: "/images/NT-ss.PNG",
-    url: "https://infinite-shore-79977.herokuapp.com/",
-    dscr: "A web application designed to create, save, and delete notes."
-  },{
-    name: "Weather Dashboard",
-    screenshot: "/images/WD-ss.PNG",
-    url: "https://alexjoeldelgado.github.io/WeatherDashboard/",
-    dscr: "A web application designed to display current and future weather conditions for any given city."
-  },{
-    name: "Work Day Planner",
-    screenshot: "/images/WDP-ss.PNG",
-    url: "https://alexjoeldelgado.github.io/WorkDayPlanner/",
-    dscr: "A web application designed to store a Daily To-Do list and show past, present, and future events."
-  },{
-    name: "Multiple Choice Quiz",
-    screenshot: "/images/CQ-ss.PNG",
-    url: "https://alexjoeldelgado.github.io/CodeQuiz/",
-    dscr: "A dynamically created Multiple Choice Quiz with a Timer and Scoreboard."
-  },{
-    name: "Password Generator",
-    screenshot: "/images/PG-ss.PNG",
-    url: "https://alexjoeldelgado.github.io/PasswordGenerator/",
-    dscr: "A web application designed to generate a password with set parameters."
-  }];
+    urlG: "https://github.com/alexjoeldelgado/NoteTaker",
+    urlD: "https://infinite-shore-79977.herokuapp.com/",
+    dscr: "A web application designed to create, save, and delete notes.",
+    tech: "LocalStorage, HTML"
+  }
+  // {
+  //   name: "Weather Dashboard",
+  //   screenshot: "/images/WD-ss.PNG",
+  //   url: "https://alexjoeldelgado.github.io/WeatherDashboard/",
+  //   dscr: "A web application designed to display current and future weather conditions for any given city."
+  // },{
+  //   name: "Work Day Planner",
+  //   screenshot: "/images/WDP-ss.PNG",
+  //   url: "https://alexjoeldelgado.github.io/WorkDayPlanner/",
+  //   dscr: "A web application designed to store a Daily To-Do list and show past, present, and future events."
+  // },{
+  //   name: "Multiple Choice Quiz",
+  //   screenshot: "/images/CQ-ss.PNG",
+  //   url: "https://alexjoeldelgado.github.io/CodeQuiz/",
+  //   dscr: "A dynamically created Multiple Choice Quiz with a Timer and Scoreboard."
+  // },{
+  //   name: "Password Generator",
+  //   screenshot: "/images/PG-ss.PNG",
+  //   url: "https://alexjoeldelgado.github.io/PasswordGenerator/",
+  //   dscr: "A web application designed to generate a password with set parameters."
+  // }
+];
 
 module.exports = portObjects;
